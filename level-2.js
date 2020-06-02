bed475fea0eec7a65de7b3afa86f3afac58aee2d
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-06-02 14:09:09 +0200

    level 2 question 1

--- level-2.js
+++ level-2.js
@@ -1,7 +1,9 @@
 // Question 1: Using the loop from question 5 above, only log the counter variable if it is an even number.
-
-// Hint:
-// You can use the modulus % operator for this.
+for (var i = 15; i <= 25; i++) {
+  if (i % 2 === 0) {
+    console.log(i);  
+  }
+}
 
 
 // Question 2: Create a function that logs the sentence "I am a function".
